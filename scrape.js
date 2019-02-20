1d8f86532df58c34b131f01137765f49b9b304ef
Ringo <ringo_kam@hotmail.com>
2019-02-19 23:50:51 -0500

    10 tab at once

--- scrape.js
+++ scrape.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const util = require("util");
-const fixPage = 10;
+const _ = require("lodash");
+const tabSize = 10;
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -19,34 +19,46 @@ const fixPage = 10;
 
     console.log("Last Page is", lastPageNumber); //lets stop it and see what we get
 
-    const pagesResult = {};
-    for (let i = 1; i <= fixPage; i++) { 
-        //do this for each page...
-        await page.goto(`https://issuetracker.unity3d.com/?page=${i}`);
-        console.log("i am on page ", i);
-        let result = await page.evaluate(() => {
-            const issueList = document.querySelectorAll(".g12.nest.idea.rel");
-            const issueListCount = issueList.length;
-            const pResult = Array(issueListCount).fill().map((d, i) => {
-                const issue = issueList[i];
-                //hmmmm, what do you think 
-                let r = {
-                    status: issue.querySelector(".status").textContent, //status
-                    count: issue.querySelector(".count").textContent, //count 
-                    title: issue.querySelector(".cn.tdn").text, //title
-                    url: issue.querySelector(".cn.tdn").href, //url
-                    category: issue.querySelector("p:nth-child(1) > span > a").text, // category
-                    date: issue.querySelector("p:nth-child(3)").textContent,  //date
-                    version: issue.querySelector("p:nth-child(5)").textContent, //version 
-                    detail: issue.querySelector(".bulk.mb0.clear").textContent, //detai
-                };
-                return r;
+    const group = Math.ceil(parseInt(100) / tabSize);
+
+    for (let i = 0; i <= group; i++) {
+        const tasks = Array(group).fill().map(async (d, ii) => {
+            const pageNumber = (i * tabSize) + ii + 1;
+            if (pageNumber > lastPageNumber) {
+                return;
+            }
+            const newPage = await browser.newPage();
+            console.log("going to ", pageNumber);
+            await newPage.goto(`https://issuetracker.unity3d.com/?page=${pageNumber}`);
+            console.log("i am on ", pageNumber);
+            let result = await newPage.evaluate(() => {
+                const issueList = document.querySelectorAll(".g12.nest.idea.rel");
+                const issueListCount = issueList.length;
+                const pResult = Array(issueListCount).fill().map((d, i) => {
+                    const issue = issueList[i];
+                    //hmmmm, what do you think 
+                    let r = {
+                        status: issue.querySelector(".status").textContent.trim(), //status
+                        count: issue.querySelector(".count").textContent, //count 
+                        title: issue.querySelector(".cn.tdn").text, //title
+                        url: issue.querySelector(".cn.tdn").href, //url
+                        category: issue.querySelector("p:nth-child(1) > span > a").text, // category
+                        date: issue.querySelector("p:nth-child(3)").textContent,  //date
+                        version: issue.querySelector("p:nth-child(5)").textContent, //version 
+                        detail: issue.querySelector(".bulk.mb0.clear").textContent, //detai
+                    };
+                    return r;
+                })
+                return pResult; //naming stuff is hard
             })
-            //this is declaring a key value pair, with i as key, and our loop result as value
-            console.log(pResult);
-            return pResult; //naming stuff is hard
-        })
-        pagesResult[i] = result; //naming stuff is hard
+            await newPage.close();
+            return result;
+        });
+        const taskResults = await Promise.all(tasks);
+        const flattenedTaskResults = _.flatten(taskResults);
+        fs.writeFile(`./public/data${i}.json`, JSON.stringify(flattenedTaskResults), () => {
+            console.info(`data${i} written!`);
+        });
     }
-    return util.promisify(fs.writeFile)("./public/data.json", JSON.stringify(pagesResult)).finally(() => browser.close());
+    await browser.close();
 })();
