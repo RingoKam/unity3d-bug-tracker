3a4c7a9971a710ffeb4edd4d3a9433d04d1f5ef1
ringo kam <ringo_kam@hotmail.com>
2019-02-26 00:29:27 -0500

    added dynamic generate template

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,57 @@
  */
 
 // You can delete this file if you're not using it
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+const _  = require("lodash");
+const path = require("path");
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+  
+    return new Promise((resolve, reject) => {
+      const versionTemplate = path.resolve(`src/templates/version.js`)
+      // Query for markdown nodes to use in creating pages.
+      resolve(
+        graphql(
+          `
+            query {
+              allDataJson {
+                totalCount
+                edges {
+                    node {
+                        id
+                        status
+                        category
+                        count
+                        version
+                        title
+                    }
+                }
+              }
+            }
+          `
+        ).then(result => {
+          if (result.errors) {
+            reject(result.errors)
+          }
+          console.log(result);
+          const nodes = result.data.allDataJson.edges.map(n => n.node);
+          const groupedByVersion = _.groupBy(nodes, node => node.version);
+          const versionAvaliable = Object.keys(groupedByVersion);
+
+          // Create pages for each markdown file.
+          versionAvaliable.forEach(ver => {
+            const p = `/${ver ? ver : "undefined"}`;
+            createPage({
+              path: p,
+              component: versionTemplate,
+              context: {
+                data: groupedByVersion[ver],
+              },
+            })
+          })
+        })
+      )
+    })
+  }
---
+++ src/templates/version.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const SecondPage = context => {
+    console.log(context);
+
+    return(
+        <Layout>
+            <SEO title="Page two" />
+            <h1>Hi from the second page</h1>
+            <p>Welcome to page 2</p>
+            <Link to="/">Go back to the homepage</Link>
+        </Layout>   
+        )
+};
+
+export default SecondPage;
