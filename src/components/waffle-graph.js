08fa141613d5cbfe3bc31f3528986968cc8584c5
Ringo <ringo_kam@hotmail.com>
2019-11-03 14:08:49 -0500

    waffle is done 🧇

---
+++ src/components/waffle-graph.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { ResponsiveWaffle } from "@nivo/waffle"
+import { groupBy, uniq } from "lodash";
+import { limitStringLength } from "../helpers/data-format"
+import { colorPalette } from "../get-color";
+
+const WaffleGraph = ({data}) => {
+    const groupByColor = groupBy(data, "color");
+    const chartData = Object.keys(groupByColor).map(color => {
+        const rows = groupByColor[color];
+        const status = uniq(rows.map(row => row.status)).join(", ");
+        const colorType = rows[0].colorType;
+        const percent = (rows.length / data.length) * 100;
+        return {
+            id: status,
+            label: status,
+            value: limitStringLength(percent, 2),
+            color: color,
+            colorType
+        }
+    }).sort((a,b) => { return a.other === b.other ? 0 : -1 });
+    
+    console.log(chartData);
+
+    return (
+        <ResponsiveWaffle
+            data={chartData}
+            rows={10}
+            columns={10}
+            padding={1}
+            total={100}
+            colors={{ datum: 'color' }}
+            margin={{ top: 10, right: 10, left: 10, bottom: 10}}>
+        </ResponsiveWaffle>
+    )
+}
+
+export default WaffleGraph
--- src/templates/version.js
+++ src/templates/version.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
 import { groupBy } from "lodash";
-import { Pane } from "evergreen-ui"
+import { Pane, Card } from "evergreen-ui"
 import { colorPalette, defaultConfig } from "../get-color";
 
 import Radar from "../components/radar";
@@ -13,17 +13,32 @@ import HeaderStatusBar from "../components/header-status-bar";
 import Stackbar from "../components/stack-bar";
 import CategoryTagInput from "../components/category-tag-input";
 import ColorConfig from "../components/color-config";
+import WaffleGraph from "../components/waffle-graph";
 
 const SecondPage = d => {
     const title = d["*"];
     const { data, releaseDate, releaseUrl } = d.pageContext;
+    const total = data.length;
     //Get a list of unqiue category, allow user to select what category they care about...
     const initalState = Object.keys(groupBy(data, "category"));
     const availableColor = Object.keys(colorPalette);
-
+    
     const [ categories, setcategories ] = useState(initalState);
     const [ colorConfig, setColorConfig ] = useState(defaultConfig);
     
+    //update data base on our config... 
+    const categoriesDict = categories.reduce((a,c) => { a[c] = true; return a; }, {});
+    const filteredData = data.filter(d => categoriesDict[d.category]);
+    const filteredDataWithColor = filteredData.map(row => {
+        const colorType = colorConfig[row.status];
+        const color = colorPalette[colorType];
+        return {
+            ...row,
+            colorType,
+            color
+        }
+    })
+
     return (
         <Layout>
             <SEO title={title} />
@@ -40,6 +55,11 @@ const SecondPage = d => {
                     categories={categories} 
                     setcategories={setcategories}/>
             </Pane>
+            <Card height={300}>
+                <WaffleGraph 
+                    data={filteredDataWithColor}
+                    />
+            </Card>
             {/* <Stackbar data={data}/> */}
             {/* <HeaderStatusBar data={data}/> */}
             {/* <VersionTable data={data} height={250}/> */}
